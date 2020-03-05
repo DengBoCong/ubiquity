4f9c96f7acd1cb857d03b2e1493f54ef8d92aedb
DBC <1210212670@qq.com>
2020-03-05 11:12:43 +0800

    'initproject'

---
+++ src/main/java/com/dbc/ubiquity/UbiquityApplication.java
@@ -0,0 +1,13 @@
+package com.dbc.ubiquity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UbiquityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UbiquityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbc/ubiquity/UbiquityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbc.ubiquity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbiquityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
